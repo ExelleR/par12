268b2888bc7f001e5c338f48972cceb826b06999
Student <student@localhost.localdomain>
2019-08-02 11:03:30 +0300

    First commit

---
+++ src/main/java/com/example/bilbi/demowiski/DemoWiskiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bilbi.demowiski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWiskiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWiskiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bilbi/demowiski/DemoWiskiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.bilbi.demowiski;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoWiskiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
